c6f899879f594b43112f4506159823e803eff727
Jakub <jakub.kucaba@gmail.com>
2023-07-28 16:33:49 +0200

    using the firestore database to store info about the user

--- app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
@@ -7,6 +7,8 @@ import android.view.WindowManager
 import android.widget.Toast
 import com.example.projemanag.R
 import com.example.projemanag.databinding.ActivitySignUpBinding
+import com.example.projemanag.firebase.FirestoreClass
+import com.example.projemanag.models.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
@@ -24,6 +26,18 @@ class SignUpActivity : BaseActivity() {
         )
 
         setupActionBar()
+    }
+    fun userRegisteredSuccess(){
+        Toast.makeText(
+            this,
+            "You have been registered.",
+            Toast.LENGTH_LONG
+        ).show()
+        hideProgressDialog()
+
+        FirebaseAuth.getInstance()
+            .signOut()  //just for now (we dont do anything with the user)
+        finish()
     }
 
     private fun setupActionBar(){  //setting up the back button
@@ -53,18 +67,12 @@ class SignUpActivity : BaseActivity() {
             showProgressDialog(resources.getString(R.string.please_wait))
             FirebaseAuth.getInstance()
                 .createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-                    hideProgressDialog()
+
                     if (task.isSuccessful) {
                         val firebaseUser: FirebaseUser = task.result!!.user!!
                         val registeredEmail = firebaseUser.email!!
-                        Toast.makeText(
-                            this,
-                            "$name you have been registered with $registeredEmail",
-                            Toast.LENGTH_LONG
-                        ).show()
-                        FirebaseAuth.getInstance()
-                            .signOut()  //just for now (we dont do anything with the user)
-                        finish()
+                        val user = User(firebaseUser.uid, name,registeredEmail)
+                        FirestoreClass().registerUser(this, user)
                     } else {
                         Toast.makeText(
                             this,
