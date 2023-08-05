fce57af6ee7fb3688a283ac74c2ce79f8ac93760
Jakub <jakub.kucaba@gmail.com>
2023-08-05 16:49:52 +0200

    select a board image

--- app/src/main/java/com/example/projemanag/activities/CreateBoardActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/CreateBoardActivity.kt
@@ -1,18 +1,46 @@
 package com.example.projemanag.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.example.projemanag.R
 import com.example.projemanag.databinding.ActivityCreateBoardBinding
+import com.example.projemanag.utils.Constants
+import java.io.IOException
 
 class CreateBoardActivity : AppCompatActivity() {
     private var binding : ActivityCreateBoardBinding? = null
+    private var mSelectedImageFileUri : Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateBoardBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
         setupActionBar()
+
+        binding?.ivBoardImage?.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                Constants.showImageChooser(this)
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    Constants.READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
     }
 
     private fun setupActionBar() {
@@ -29,4 +57,42 @@ class CreateBoardActivity : AppCompatActivity() {
             onBackPressed()
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == Constants.READ_STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Constants.showImageChooser(this)
+            }
+        } else {
+            Toast.makeText(
+                this,
+                "You denied access to storage. Please enable permissions",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == Constants.PICK_IMAGE_REQUEST_CODE && data!!.data  != null){
+            mSelectedImageFileUri = data.data
+
+            try {
+                Glide
+                    .with(this)
+                    .load(mSelectedImageFileUri)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_board_place_holder)
+                    .into(findViewById(R.id.iv_board_image))
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/projemanag/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/MyProfileActivity.kt
@@ -25,10 +25,6 @@ import java.io.IOException
 
 class MyProfileActivity : BaseActivity() {
 
-    companion object {
-        private const val READ_STORAGE_PERMISSION_CODE = 1
-        private const val PICK_IMAGE_REQUEST_CODE = 2
-    }
 
     private var mSelectedImageFileUri: Uri? = null
     private lateinit var mUserDetails: User
@@ -46,12 +42,12 @@ class MyProfileActivity : BaseActivity() {
 
         binding?.ivUserImageProfile?.setOnClickListener{
             if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
-                showImageChooser()
+                Constants.showImageChooser(this)
             }else {
                 ActivityCompat.requestPermissions(
                     this,
                     arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                    READ_STORAGE_PERMISSION_CODE
+                    Constants.READ_STORAGE_PERMISSION_CODE
                 )
             }
         }
@@ -74,9 +70,9 @@ class MyProfileActivity : BaseActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == READ_STORAGE_PERMISSION_CODE){
+        if(requestCode == Constants.READ_STORAGE_PERMISSION_CODE){
             if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                showImageChooser()
+                Constants.showImageChooser(this)
             }
         } else {
             Toast.makeText(
@@ -87,14 +83,9 @@ class MyProfileActivity : BaseActivity() {
         }
     }
 
-    private fun showImageChooser(){
-        var galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE_REQUEST_CODE && data!!.data  != null){
+        if(resultCode == Activity.RESULT_OK && requestCode == Constants.PICK_IMAGE_REQUEST_CODE && data!!.data  != null){
             mSelectedImageFileUri = data.data
 
             try {
@@ -172,7 +163,7 @@ class MyProfileActivity : BaseActivity() {
                 FirebaseStorage.getInstance().reference
                     .child("USER_IMAGE"
                             + System.currentTimeMillis()
-                            + "." + getFileExtension(mSelectedImageFileUri))
+                            + "." + Constants.getFileExtension(this, mSelectedImageFileUri))
 
             sRef.putFile(mSelectedImageFileUri!!).addOnSuccessListener {
                 taskSnapshot ->
@@ -202,10 +193,6 @@ class MyProfileActivity : BaseActivity() {
                 hideProgressDialog()
             }
         }
-    }
-
-    private fun getFileExtension(uri : Uri?): String?{
-        return MimeTypeMap.getSingleton().getExtensionFromMimeType(contentResolver.getType(uri!!))
     }
 
     fun profileUpdateSuccess(){
