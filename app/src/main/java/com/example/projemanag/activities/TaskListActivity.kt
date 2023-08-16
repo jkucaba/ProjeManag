4c7da8378e6ccaa125217d3663d979ddedc400be
Jakub <jakub.kucaba@gmail.com>
2023-08-16 18:59:41 +0200

    preparing the TaskList adapter

--- app/src/main/java/com/example/projemanag/activities/TaskListActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/TaskListActivity.kt
@@ -2,10 +2,13 @@ package com.example.projemanag.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.projemanag.R
+import com.example.projemanag.adapters.TaskListItemsAdapter
 import com.example.projemanag.databinding.ActivityTaskListBinding
 import com.example.projemanag.firebase.FirestoreClass
 import com.example.projemanag.models.Board
+import com.example.projemanag.models.Task
 import com.example.projemanag.utils.Constants
 
 class TaskListActivity : BaseActivity() {
@@ -30,6 +33,14 @@ class TaskListActivity : BaseActivity() {
         hideProgressDialog()
         setupActionBar(board.name)
 
+        val addTaskList = Task(resources.getString(R.string.add_list))
+
+        board.taskList.add(addTaskList)
+        binding?.rvTaskList?.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        binding?.rvTaskList?.setHasFixedSize(true)
+
+        val adapter = TaskListItemsAdapter(this, board.taskList)
+        binding?.rvTaskList?.adapter = adapter
     }
 
     private fun setupActionBar(title: String) {
