9ad10372ca6716145c5680d356f8c9edf26f2791
Jakub <jakub.kucaba@gmail.com>
2023-09-03 14:35:57 +0200

    Creating members item

--- app/src/main/java/com/example/projemanag/utils/Constants.kt
+++ app/src/main/java/com/example/projemanag/utils/Constants.kt
@@ -23,6 +23,8 @@ object Constants {
 
     const val TASK_LIST : String = "taskList"
 
+    const val BOARD_DETAIL : String = "board_detail"
+
     fun showImageChooser(activity: Activity){
         var galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         activity.startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
