b614d638bdbc850682bfeea4394b9e05f0423a03
Jakub <jakub.kucaba@gmail.com>
2023-09-03 13:03:48 +0200

    preparing the members activity

--- app/src/main/java/com/example/projemanag/activities/TaskListActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/TaskListActivity.kt
@@ -1,7 +1,10 @@
 package com.example.projemanag.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.projemanag.R
 import com.example.projemanag.adapters.TaskListItemsAdapter
@@ -121,5 +124,20 @@ class TaskListActivity : BaseActivity() {
         showProgressDialog(resources.getString(R.string.please_wait))
         FirestoreClass().addUpdateTaskList(this, mBoardDetails)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_members, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_members ->{
+                startActivity(Intent(this,MembersActivity::class.java))
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
