4248e50e8f9015b3a309cbe53f1a8801d6cf5189
Jakub <jakub.kucaba@gmail.com>
2023-09-08 20:05:12 +0200

    Fetching and Displaying the Members of a board

--- app/src/main/java/com/example/projemanag/activities/MembersActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/MembersActivity.kt
@@ -2,12 +2,17 @@ package com.example.projemanag.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.projemanag.R
+import com.example.projemanag.adapters.MemberListItemsAdapter
 import com.example.projemanag.databinding.ActivityMembersBinding
+import com.example.projemanag.firebase.FirestoreClass
 import com.example.projemanag.models.Board
+import com.example.projemanag.models.User
 import com.example.projemanag.utils.Constants
 
-class MembersActivity : AppCompatActivity() {
+class MembersActivity : BaseActivity() {
 
     var binding : ActivityMembersBinding? = null
 
@@ -23,6 +28,18 @@ class MembersActivity : AppCompatActivity() {
         }
 
         setupActionBar()
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().getAssignedMembersListDetails(this, mBoardDetails.assignedTo)
+    }
+
+    fun setupMembersList(list: ArrayList<User>) {
+        hideProgressDialog()
+        findViewById<RecyclerView>(R.id.rv_members_list).layoutManager = LinearLayoutManager(this)
+        findViewById<RecyclerView>(R.id.rv_members_list).setHasFixedSize(true)
+
+        val adapter = MemberListItemsAdapter(this, list)
+        findViewById<RecyclerView>(R.id.rv_members_list).adapter = adapter
     }
 
     private fun setupActionBar() {
--- app/src/main/java/com/example/projemanag/utils/Constants.kt
+++ app/src/main/java/com/example/projemanag/utils/Constants.kt
@@ -25,6 +25,8 @@ object Constants {
 
     const val BOARD_DETAIL : String = "board_detail"
 
+    const val ID: String = "id"
+
     fun showImageChooser(activity: Activity){
         var galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         activity.startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
